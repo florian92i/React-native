889b7c519dde69d53c4859c3e14f82a619c0fc9f
Florian <florian.compiegne@eemi.com>
2018-05-29 23:28:43 +0200

    ajoute de tout

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name} ok ok!</Text>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
+
+// On a crée une classe Greeting qui marche comme un systeme de composant et qui va donc crée un template pour ensuite l'afficher
+//On pressise le nom avec Greeting name='Rexxar' 
