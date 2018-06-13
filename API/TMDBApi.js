ced891b4996b5b278d2d0c050a4113dcdde7ed7a
Florian <compiegne92@gmail.com>
2018-06-13 11:24:58 -0700

    scrool api

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,10 +1,16 @@
 // API/TMDBApi.js
-
+// dossier ou on defini les fonctions qui vont permettre d'echanger avec l'api
 const API_TOKEN = "d474f24b444d730aa307390bbca2299f";
 
-export function getFilmsFromApiWithSearchedText (text) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
   return fetch(url)
     .then((response) => response.json())
     .catch((error) => console.error(error))
+}
+
+// API/TMDBApi.js
+
+export function getImageFromApi (name) {
+  return 'https://image.tmdb.org/t/p/w300' + name
 }
--- components/Search.js
+++ components/Search.js
@@ -7,38 +7,116 @@ import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi' // import { } f
 
 
 
-import { StyleSheet, FlatList, View, TextInput, Button, Text } from 'react-native'
+import { StyleSheet, FlatList, View, TextInput, Button, Text, ActivityIndicator } from 'react-native'
 
 class Search extends React.Component {
 // On initialise un tableau vide pour l'appeler apres dans _loadFilms
   constructor(props) {
     super(props)
-    this.state = { films: [] }
+    this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
+    this.page = 0 // Compteur pour connaître la page courante
+    this.totalPages = 0 // Nombre de pages totales pour savoir si on a atteint la fin des retours de l'API TMDB
+    this.state = {
+      films: [],
+      isLoading: false, // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche
+      nosearch:false,
+     }
   }
+//---------------------------------- load & if pas de film ----------------------------------//
+  _displayLoading() {
+        if (this.state.isLoading) {
+          return (
+            <View style={styles.loading_container}>
+              <ActivityIndicator size='large' />
+              {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+            </View>
+          )
+        }
+      }
 
+  _displayFilm() {
+            if (this.state.films.length < 1 && this.state.nosearch == true) {
+              return (
+                <View style={styles.loading_container}>
+                  <Text>Aucune recherche trouvé</Text>
+                  {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+                </View>
+              )
+            }
+          }
+//---------------------------------- function pour utiliser l'api ----------------------------------//
 
-  _loadFilms() {
+//permet
+_searchFilms() {
+ this.page = 0
+ this.totalPages = 0
+ this.setState({ //setState  possède un paramètre  callback  qui permet d'exécuter une action dès que notre state a fini de se mettre à jour
+   films: [],
+ }, () => {
+     //console.log("Page : " + this.page + " / TotalPages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
+     this._loadFilms()
+ })
+}
 
-    getFilmsFromApiWithSearchedText("star").then(data => {
-      // on utiliser la function stocker dans le TMDBApi lorsque que getFilmsFromApiWithSearchedText se fini callback et on remplie le tableau vide du
-      // resultats de l'api
-      this.setState({ films: data.results })
-    })
+ _loadFilms() {
+  // console.log(this.searchedText) // Un log pour vérifier qu'on a bien le texte du TextInput
+   if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+     this.setState({ isLoading: true }) // Lancement du chargement
+     getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => { //On incremente la page quand on appelle la function pour recupere au debut la 1er ensuite 2 ect ...
+       //  console.log(data);
+       // on utiliser la function stocker dans le TMDBApi lorsque que getFilmsFromApiWithSearchedText se fini callback et on remplie le tableau vide du
+       // resultats de l'api
+         this.page = data.page
+         this.totalPages = data.total_pages
+         this.setState({
+           films: [ ...this.state.films, ...data.results ], // syntaxe es6 ...this.state.films, ...data.results = films: this.state.films.concat(data.results)
+           isLoading: false, // Arrêt du chargement
+           nosearch:true, //la premiere recherche va passer nosearch a true en continu
+         })
+     })
+   }
  }
+
+ _searchTextInputChanged(text) {
+  this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
+}
+
+//---------------------------------- Ce qu'il va s'afficher a l'ecran ----------------------------------//
+
   render() {
+  //  console.log("RENDER")
+
       return (
         <View style={styles.main_container}>
-         <TextInput style={styles.textinput} placeholder='Titre du film'/>
-         <Button style={{ height: 50 }} title='Rechercher' onPress={() => this._loadFilms()}/>
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._loadFilms()} //le bouton "Envoyer" du clavier pour valider un TextInput
+
+        />
+         <Button style={{ height: 50 }} title='Rechercher' onPress={() => this._searchFilms()}/>
          <FlatList
            data={this.state.films}
            keyExtractor={(item) => item.id.toString()}
            renderItem={({item}) => <FilmItem film={item}/>}
+           onEndReachedThreshold={0.2} //
+           onEndReached={() => {
+              if (this.state.films.length > 0 && this.page < this.totalPages) { // On vérifie également qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
+                 this._loadFilms()
+              }
+          }}
          />
+         {this._displayLoading()}
+
+         {this._displayFilm()}
+
        </View>
       )
    }
 }
+
+//---------------------------------- le css ----------------------------------//
 
 const styles = StyleSheet.create({
   main_container: {
@@ -52,7 +130,17 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
-  }
+  },
+
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 })
 
 export default Search
