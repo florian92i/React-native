121054070c716e581c33f34926329a98be1fa38d
Florian <florian.compiegne@eemi.com>
2018-06-28 23:43:57 +0200

    personnalisation de la barre de navigation du bas

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,11 +1,12 @@
 // Navigation/Navigation.js
 // Contient toute la navigation de l'app donc une vue = une nouvelle route dans ce fichier
+import React from 'react'
 
 import { createStackNavigator, createBottomTabNavigator  } from 'react-navigation'
 import Search from '../components/Search'
+import { StyleSheet, Image } from 'react-native';
 import FilmDetail from '../components/FilmDetail'
 import Favorites from '../components/Favorites'
-
 
 const SearchStackNavigator = createStackNavigator({
   Search: {
@@ -19,14 +20,45 @@ const SearchStackNavigator = createStackNavigator({
   }
 })
 
-const MoviesTabNavigator = createBottomTabNavigator({
-  Search: {
-    screen: SearchStackNavigator //  ajoute une navigation dans un onglet et faire ce qu'on appelle une combinaison de navigation 
+const MoviesTabNavigator = createBottomTabNavigator(
+  {
+    Search: {
+      screen: SearchStackNavigator,    //  ajoute une navigation dans un onglet et faire ce qu'on appelle une combinaison de navigation 
+      navigationOptions: {
+        tabBarIcon: () => { // On définit le rendu de nos icônes par les images récemment ajoutés au projet
+          return <Image
+            source={require('../Images/ic_search.png')}
+            style={styles.icon}/> // On applique un style pour les redimensionner comme il faut
+        }
+      }
+    },
+    Favorites: {
+      screen: Favorites,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+            source={require('../Images/ic_favorite.png')}
+            style={styles.icon}/>
+        }
+      }
+    }
   },
-  Favorites: {
-    screen: Favorites
+  {
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+      inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+      showLabel: false, // On masque les titres
+      showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+    }
+  }
+)
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
   }
 })
-
 
 export default MoviesTabNavigator
